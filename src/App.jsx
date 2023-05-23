3821087e39b1d2ecc315cb5012bf4407ecece8e7
Lúcia Duarte <analuciacfduarte@gmail.com>
2023-05-23 20:03:31 +0100

    update starter code in app.jsx and app.css

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>IronContacts</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>INSERT IMAGE HERE</td>
+            <td>INSERT NAME HERE</td>
+            <td>INSERT POPULARITY HERE</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
